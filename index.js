643d8449a1d70d865ef2de429d38c23e0470c706
tahiee <niazi.tahirkhan15@gmail.com>
2024-02-20 19:31:15 +0500

    added

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+require('dotenv').config()
 const app = express();
-const port = 4000;
+// const port = 4000;
+
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! kia hal hai");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.get("/login", (req, res) => {
+  res.send(`login route my mera name ${process.env.MYUSERNAME}`);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
